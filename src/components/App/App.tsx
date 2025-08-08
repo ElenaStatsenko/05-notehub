dc4067cfc57bc9214b1cee21a9ac8c772c02f7c4
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2025-08-08 22:17:58 +0300

    создала проект и настроила его

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+
+
+export default function App() {
+  
+
+  return (
+    <>
+     
+        
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+
