bdd3e71246aa8f3095a20e8925da60e65a519e01
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2025-08-10 16:03:46 +0300

    сделала notelist

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@
 import css from './App.module.css';
 import { useQuery } from '@tanstack/react-query';
 import { fetchNotes } from '../../services/noteService';
+import Notelist from '../NoteList/NoteList';
 
 export default function App() {
   const {data} = useQuery({
@@ -9,12 +10,12 @@ export default function App() {
   queryFn: () => fetchNotes(),   
   
 });
-console.log(data?.notes) 
+
 
   return (
     <div className={css.app}>
 	<header className={css.toolbar}>
-    <p> Hello </p>
+    <Notelist notes={data?.notes}/>
 	
   </header>
 </div>
