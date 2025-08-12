3c024d0a788027a3ab8e61cd83fa19e73c1dddf0
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2025-08-12 16:07:35 +0300

    сделала модалку (открытие, закрытие.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,36 +1,43 @@
+import css from "./App.module.css";
 
-import css from './App.module.css';
-
-import { useQuery } from '@tanstack/react-query';
-import { useState} from "react";
-import Pagination from '../Pagination/Pagination';
-import { fetchNotes } from '../../services/noteService';
-import Notelist from '../NoteList/NoteList';
-
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import { useState } from "react";
+import Pagination from "../Pagination/Pagination";
+import { fetchNotes } from "../../services/noteService";
+import Notelist from "../NoteList/NoteList";
+import Modal from "../Modal/Modal";
 
 export default function App() {
   const [page, setPage] = useState(1);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const {data, isSuccess} = useQuery({
-  queryKey: ['myFetchKey', page], 
-  queryFn: () => fetchNotes(page),   
-  
-});
+  const { data, isSuccess } = useQuery({
+    queryKey: ["myFetchKey", page],
+    queryFn: () => fetchNotes(page),
+    placeholderData: keepPreviousData,
+  });
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
   return (
     <div className={css.app}>
-	<header className={css.toolbar}>
-    {isSuccess && data.totalPages > 1 &&( <Pagination 
-    pageCount={data?.totalPages ?? 0}
-          page={page}
-          setPage={setPage}/> )}
-    
-  </header>
-  <Notelist notes={data?.notes}/>
-	
-</div>
-  )
+      <header className={css.toolbar}>
+        {isSuccess && data.totalPages > 1 && (
+          <Pagination
+            pageCount={data?.totalPages ?? 0}
+            page={page}
+            setPage={setPage}
+          />
+        )}
+        <button className={css.button} onClick={openModal}>Create note +</button>
+      </header>
+      <Notelist notes={data?.notes} />
+      {isModalOpen && <Modal onClose={closeModal} />}
+    </div>
+  );
 }
-
-
