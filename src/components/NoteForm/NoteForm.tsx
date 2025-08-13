3c4d2f70bb2eb15ce372e9637c48e70dff417e52
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2025-08-13 13:13:40 +0300

    префакторинг формы с помощью Формик

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -1,48 +1,68 @@
 import css from "./NoteForm.module.css";
-
+import { Formik, Form, Field, FormikHelpers } from "formik";
 interface NoteFormProps {
   onCancel: () => void;
 }
+interface ValuesFormProps {
+  title: string;
+  content: string;
+  tag: "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
+}
+const valuesForm: ValuesFormProps = {
+  title: "назва",
+  content: "",
+  tag: "Todo",
+};
+
 export default function NoteForm({ onCancel }: NoteFormProps) {
+  const handleSubmit = (
+    values: ValuesFormProps,
+    actions: FormikHelpers<ValuesFormProps>
+  ) => {
+    console.log("Order data:", values);
+    actions.resetForm();
+  };
   return (
-    <form className={css.form}>
-      <div className={css.formGroup}>
-        <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" className={css.input} />
-        <span name="title" className={css.error}></span>
-      </div>
+    <Formik initialValues={valuesForm} onSubmit={handleSubmit}>
+      <Form className={css.form}>
+        <div className={css.formGroup}>
+          <label htmlFor="title">Title</label>
+          <Field id="title" type="text" name="title" className={css.input} />
+          <span name="title" className={css.error}></span>
+        </div>
 
-      <div className={css.formGroup}>
-        <label htmlFor="content">Content</label>
-        <textarea
-          id="content"
-          name="content"
-          rows={8}
-          className={css.textarea}
-        />
-        <span name="content" className={css.error} />
-      </div>
+        <div className={css.formGroup}>
+          <label htmlFor="content">Content</label>
+          <Field
+            id="content"
+            name="content"
+            rows={8}
+            className={css.textarea}
+          />
+          <span name="content" className={css.error} />
+        </div>
 
-      <div className={css.formGroup}>
-        <label htmlFor="tag">Tag</label>
-        <select id="tag" name="tag" className={css.select}>
-          <option value="Todo">Todo</option>
-          <option value="Work">Work</option>
-          <option value="Personal">Personal</option>
-          <option value="Meeting">Meeting</option>
-          <option value="Shopping">Shopping</option>
-        </select>
-        <span name="tag" className={css.error} />
-      </div>
+        <div className={css.formGroup}>
+          <label htmlFor="tag">Tag</label>
+          <Field as="select" id="tag" name="tag" className={css.select}>
+            <option value="Todo">Todo</option>
+            <option value="Work">Work</option>
+            <option value="Personal">Personal</option>
+            <option value="Meeting">Meeting</option>
+            <option value="Shopping">Shopping</option>
+          </Field>
+          <span name="tag" className={css.error} />
+        </div>
 
-      <div className={css.actions}>
-        <button type="button" onClick={onCancel} className={css.cancelButton}>
-          Cancel
-        </button>
-        <button type="submit" className={css.submitButton} disabled={false}>
-          Create note
-        </button>
-      </div>
-    </form>
+        <div className={css.actions}>
+          <button type="button" onClick={onCancel} className={css.cancelButton}>
+            Cancel
+          </button>
+          <button type="submit" className={css.submitButton} disabled={false}>
+            Create note
+          </button>
+        </div>
+      </Form>
+    </Formik>
   );
 }
